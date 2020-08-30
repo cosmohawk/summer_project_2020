1af796240766b9f74ba8ca811f8bfa00d5f60c01
Robert Webster <rwhwebster@gmail.com>
2020-08-30 16:40:39 +0100

    added function vectorize_wordlists()

--- src/topics/hashtag_analysis.py
+++ src/topics/hashtag_analysis.py
@@ -1,5 +1,10 @@
 import sys
 
+import pandas as pd
+
+from sklearn import feature_extraction as skfe
+from sklearn import decomposition as skd
+
 def get_ranked_wordcounts(words):
     '''
     Use the Counter class from Python collections to count the frequency
@@ -31,4 +36,29 @@ def get_ranked_wordcounts(words):
 
     return counts_sorted
 
+def vectorize_wordlists(words_lists):
+    '''
+    Generate sparse matrix of word-counts from Pandas series where each entry 
+    is a list of words.
+
+    Parameters
+    ----------
+    words_lists : Pandas Series
+        A series of lists containing words to be one-hot encoded.
+
+    Returns
+    -------
+    observations : Pandas DataFrame.sparse
+        A sparse dataframe, where each row is a vectorised representation 
+        of a document.
+    '''
+    cv = skfe.CountVectorizer()
+    # Turn each list into one space-separated string
+    corpus = words_lists.apply(lambda x: ' 'join(eval(x)) if isinstance(x, str) else ' '.join(x))
+    # Apply count vectorisation to documents
+    corpvec = cv.fit_transform(corpus)
+    # Put results into sparse dataframe
+    observations = pd.DataFrame.sparse.from_spmatrix(corpvec, index = words_lists.index, columns=cv.get_feature_names())
+
+    return observations
 
